ac44911eb9bac986b82dbac982525f2c0c164625
htran <htran@aquaveo.com>
2020-09-11 10:09:09 -0600

    - Update version to 0.1.3 - Use sdist instead

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name=release_package,
-    version='0.1.2',
+    version='0.1.3',
     description='django_param provides ParamForm class which allows python param to be used in django form.',
     # other arguments omitted
     long_description=long_description,
