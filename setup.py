a2d31c66321dc2e982997c69e476c6af8df1c7ec
nswain <nswain@aquaveo.com>
2020-09-15 12:15:45 -0600

    Fix build to include HTML, CSS, and JS files.

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(
     name=release_package,
     version='0.1.6',
     description='django_param provides ParamForm class which allows python param to be used in django form.',
-    # other arguments omitted
     long_description=long_description,
     long_description_content_type='text/x-rst',
     keywords='',
@@ -27,7 +26,6 @@ setup(
     url='https://github.com/Aquaveo/django_param',
     license='BSD 2-Clause License',
     packages=find_namespace_packages(),
-    package_data={},
     include_package_data=True,
     zip_safe=False,
     install_requires=requirements,
