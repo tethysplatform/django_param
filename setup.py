8f6b32d2880d575817f50c3fb4544558a5c3c2e1
nswain <nswain@aquaveo.com>
2020-10-09 16:47:41 -0600

    Increment version to test CI after moving repo to Tethys Platform org on GitHub.

--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name=release_package,
-    version='0.1.8',
+    version='0.1.9',
     description='django_param provides ParamForm class which allows python param to be used in django form.',
     long_description=long_description,
     long_description_content_type='text/x-rst',
     keywords='',
     author='htran',
     author_email='htran@aquaveo.com',
-    url='https://github.com/Aquaveo/django_param',
+    url='https://github.com/tethysplatform/django_param',
     license='BSD 2-Clause License',
     packages=find_namespace_packages(),
     include_package_data=True,
